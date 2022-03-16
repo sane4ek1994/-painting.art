e4c46819d1e01feabe9a825fdc012c3ea4034399
Alexandr Naumov <63644905+sane4ek1994@users.noreply.github.com>
2022-03-16 12:53:41 +0300

    forms-add

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
+// const dist = "./dist/";
+const dist = "C:/MAMP/htdocs/test";
+
+
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
